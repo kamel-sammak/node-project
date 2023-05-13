b0a499fd37486b3ce4a7d82d22753ef2701177da
kamel-sammak <kamel.sammak@gmail.com>
2023-05-13 13:53:01 +0300

    first commit

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -3,20 +3,30 @@ const doctorSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true, "please enter user name !"]
+            required: [true, "please enter name !"]
         },
         specialty: {
             type: String,
-            required: [true, "please enter user name !"]
+            required: [true, "please enter specialty !"]
         },
         gender: {
             type: String,
-            required: [true, "please enter user name !"]
+            required: [true, "please enter gender !"]
         },
         age: {
             type: Number,
-            required: [true, "please enter user name !"]
+            required: [true, "please enter age !"]
         },
+        contact:{
+            email:{
+                type: String,
+                required: [true , "please enter email !"]
+            },
+            phone:{
+                type: Number,
+                required: [true , "please enter phone !"]
+            }
+        }
     },
     {
         timestamps: true
